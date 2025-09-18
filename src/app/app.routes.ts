b857155e9c8ab1dcad815164bc49876fa822d1c3
Mhmdelthokaby <melthokaby22@gmail.com>
2025-09-19 01:36:11 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { Home } from '../app/core/components/home/home';
+import { LoginComponent } from '../app/core/components/login/login';
+import { SignupComponent } from '../app/core/components/signup/signup';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+
+];
